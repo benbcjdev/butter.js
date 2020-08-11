ff5b4b45e053039dd147bf905457cd4d3abf545b
bcjmoments <bcjmoments@users.noreply.github.com>
2020-08-11 12:18:59 -0500

    changed to self-invoking function

--- butter.js
+++ butter.js
@@ -1,105 +1,106 @@
 // butter.js
 
-var Butter = function() {
+(function(root){
+    var Butter = function() {
 
-    var self = this;
+        var self = this;
 
-    this.defaults = {
-        wrapperId: 'butter',
-        wrapperDamper: 0.08,
-        cancelOnTouch: false
-    }
-    
-    this.validateOptions = function(ops) {
-        for (var prop in ops) {
-            if (self.defaults.hasOwnProperty(prop)) {
-                Object.defineProperty(self.defaults, prop, {value: Object.getOwnPropertyDescriptor(ops, prop).value})
-                console.log(prop + ', ' + Object.getOwnPropertyDescriptor(self.defaults, prop).value);
+        this.defaults = {
+            wrapperId: 'butter',
+            wrapperDamper: 0.07,
+            cancelOnTouch: false
+        }
+        
+        this.validateOptions = function(ops) {
+            for (var prop in ops) {
+                if (self.defaults.hasOwnProperty(prop)) {
+                    Object.defineProperty(self.defaults, prop, {value: Object.getOwnPropertyDescriptor(ops, prop).value})
+                }
             }
         }
-    }
 
-    this.wrapperDamper;
-    this.wrapperId;
-    this.cancelOnTouch;
-    this.wrapper;
-    this.wrapperOffset = 0;
-    this.animateId;
-    this.resizing = false;
-    this.active = false;
-};
-
-Butter.prototype = {
-
-    init: function(options) {
-        if (options) {
-            this.validateOptions(options);
-        }
-
-        this.active = true;
+        this.wrapperDamper;
+        this.wrapperId;
+        this.cancelOnTouch;
+        this.wrapper;
+        this.wrapperOffset = 0;
+        this.animateId;
         this.resizing = false;
-        this.wrapperDamper = this.defaults.wrapperDamper;
-        this.wrapperId = this.defaults.wrapperId;
-        this.cancelOnTouch = this.defaults.cancelOnTouch;
+        this.active = false;
+    };
 
-        this.wrapper = document.getElementById(this.wrapperId);
-        this.wrapper.style.position = 'fixed';
-        this.wrapper.style.width = '100%';
+    Butter.prototype = {
 
-        document.body.style.height = this.wrapper.clientHeight + 'px';
+        init: function(options) {
+            if (options) {
+                this.validateOptions(options);
+            }
 
-        window.addEventListener('resize', this.resize.bind(this));
-        if (this.cancelOnTouch) {
-            window.addEventListener('touchstart', this.cancel.bind(this));
-        }
-        this.wrapperOffset = window.scrollY;
-        this.animateId = window.requestAnimationFrame(this.animate.bind(this));
-    },
+            this.active = true;
+            this.resizing = false;
+            this.wrapperDamper = this.defaults.wrapperDamper;
+            this.wrapperId = this.defaults.wrapperId;
+            this.cancelOnTouch = this.defaults.cancelOnTouch;
 
-    wrapperUpdate: function() {
-        var scrollY = document.body.scrollTop || window.scrollY;
-        this.wrapperOffset += (scrollY - this.wrapperOffset) * this.wrapperDamper;
-        this.wrapper.style.transform = 'translate3d(0,' + (-this.wrapperOffset.toFixed(2)) + 'px, 0)';
-    },
+            this.wrapper = document.getElementById(this.wrapperId);
+            this.wrapper.style.position = 'fixed';
+            this.wrapper.style.width = '100%';
 
-    resize: function() {
-        var self = this;
-        if (!self.resizing) {
-            console.log('resize called');
-            self.resizing = true;
-            cancelAnimationFrame(self.animateId);
-            window.setTimeout(function() {
-                if (parseInt(document.body.style.height) != parseInt(self.wrapper.clientHeight)) {
-                    document.body.style.height = self.wrapper.clientHeight + 'px';
-                }
-                animateId = window.requestAnimationFrame(self.animate.bind(self));
-                self.resizing = false;
-            }, 150)
-        }
-    },
+            document.body.style.height = this.wrapper.clientHeight + 'px';
 
-    animate: function() {
-        this.wrapperUpdate();
-        this.animateId = requestAnimationFrame(this.animate.bind(this));
-    },
+            window.addEventListener('resize', this.resize.bind(this));
+            if (this.cancelOnTouch) {
+                window.addEventListener('touchstart', this.cancel.bind(this));
+            }
+            this.wrapperOffset = window.scrollY;
+            this.animateId = window.requestAnimationFrame(this.animate.bind(this));
+        },
 
-    cancel: function() {
-        if (this.active) {
-            cancelAnimationFrame(this.animateId);
+        wrapperUpdate: function() {
+            var scrollY = document.body.scrollTop || window.scrollY;
+            this.wrapperOffset += (scrollY - this.wrapperOffset) * this.wrapperDamper;
+            this.wrapper.style.transform = 'translate3d(0,' + (-this.wrapperOffset.toFixed(2)) + 'px, 0)';
+        },
 
-            window.removeEventListener('resize', this.resize);
-            window.removeEventListener('touchstart', this.cancel);
-            this.wrapper.removeAttribute('style');
-            document.body.removeAttribute('style');
+        resize: function() {
+            var self = this;
+            if (!self.resizing) {
+                console.log('resize called');
+                self.resizing = true;
+                cancelAnimationFrame(self.animateId);
+                window.setTimeout(function() {
+                    if (parseInt(document.body.style.height) != parseInt(self.wrapper.clientHeight)) {
+                        document.body.style.height = self.wrapper.clientHeight + 'px';
+                    }
+                    animateId = window.requestAnimationFrame(self.animate.bind(self));
+                    self.resizing = false;
+                }, 150)
+            }
+        },
 
-            this.active = false;
-            this.wrapper = "";
-            this.wrapperOffset = 0;
-            this.resizing = true;
-            this.animateId = "";
-        }
-    },
-}
+        animate: function() {
+            this.wrapperUpdate();
+            this.animateId = requestAnimationFrame(this.animate.bind(this));
+        },
 
-var butter = new Butter();
-butter.init();
+        cancel: function() {
+            if (this.active) {
+                cancelAnimationFrame(this.animateId);
+
+                window.removeEventListener('resize', this.resize);
+                window.removeEventListener('touchstart', this.cancel);
+                this.wrapper.removeAttribute('style');
+                document.body.removeAttribute('style');
+
+                this.active = false;
+                this.wrapper = "";
+                this.wrapperOffset = 0;
+                this.resizing = true;
+                this.animateId = "";
+            }
+        },
+    };
+
+    root.butter = new Butter();
+    
+})(this);
