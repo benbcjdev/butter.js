708fd1e4ed835f3c14674f449b48a091618983ed
m gb <mag190033@gmail.com>
2021-07-03 15:28:32 +0200

    basic rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
     input: 'src/butter.js',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'cjs'
-    }
+    output: [
+        { file: "dist/butter.js", format: "cjs" },
+        { file: "dist/butter.min.js", format: "cjs", plugins: [terser()] },
+        { file: "dist/butter.esm.js", format: "esm" },
+    ],
+    plugins: [terser()]
   };
